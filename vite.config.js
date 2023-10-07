8429cd9d32d9ceb3425770d75c7518915347d503
raccon <tyuf64@163.com>
2023-10-07 09:50:50 +0800

    chore: 取消启动项目开启浏览器

--- vite.config.js
+++ vite.config.js
@@ -33,13 +33,13 @@ export default defineConfig({
     },
     //本地运行配置，以及反向代理配置
     server: {
-        host: "localhost",
-        https: false,//是否启用 https
-        cors: true,//为开发服务器配置 CORS , 默认启用并允许任何源
-        open: true,//服务启动时自动在浏览器中打开应用
+        host: "localhost", // 0.0.0.0
+        https: false, //是否启用 https
+        cors: true, //为开发服务器配置 CORS , 默认启用并允许任何源
+        open: false, //服务启动时自动在浏览器中打开应用
         port: 5173,
         strictPort: false, //设为true时端口被占用则直接退出，不会尝试下一个可用端口
-        hmr: false,//禁用或配置 HMR 连接
+        hmr: false, //禁用或配置 HMR 连接
         // 传递给 chockidar 的文件系统监视器选项
         watch: {
             ignored: ["!**/node_modules/your-package-name/**"]
