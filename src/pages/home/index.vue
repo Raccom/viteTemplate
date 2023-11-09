8440af0cea9101a002679e2e7cbbd6096c3ed100
Raccom <tyuf64@163.com>
2023-11-09 15:30:00 +0800

    feat: 添加动态icon的显示和样例

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,14 +1,17 @@
 <template>
     <div text-gray-500 m-15>
+        <!-- pinia 获取状态 -->
         <p text-24px mb-10px>{{ username }} home</p>
         <div>
+            <!-- ref 数据响应和传参 -->
             <n-button @click="num++" mr-5>count is {{ num }}</n-button>
             <HelloWorld v-model="num"></HelloWorld>
         </div>
     </div>
     <n-divider />
     <n-space m-15>
-        <n-button><icon-maki:aerialway color="#000" style="font-size: 18px;" /></n-button>
+        <!-- naive 组件主题展示 -->
+        <n-button>default</n-button>
         <n-button type="tertiary"> Tertiary </n-button>
         <n-button type="primary"> Primary </n-button>
         <n-button type="info"> Info </n-button>
@@ -16,16 +19,37 @@
         <n-button type="warning"> Warning </n-button>
         <n-button type="error"> Error </n-button>
     </n-space>
+    <n-divider />
+    <n-space m-15>
+        <n-button circle>
+            <template #icon>
+                <!-- 直接使用 icon -->
+                <icon-maki:aerialway color="#000" text-18px />
+            </template>
+        </n-button>
+        <n-button circle v-for="item in icons">
+            <template #icon>
+                <!-- 动态图标渲染 -->
+                <Icon :icon="item" text-18px></Icon>
+            </template>
+        </n-button>
+    </n-space>
 </template>
 
 <script setup>
 import { storeToRefs } from 'pinia';
+import { Icon } from "@iconify/vue";
 import { useStore } from '@/store';
 
 const store = useStore();
 const { username } = storeToRefs(store);
 
 const num = ref(0);
+
+const icons = [
+    'ph:address-book',
+    'ph:air-traffic-control-light'
+]
 </script>
 
 <style lang="scss" scoped></style>
